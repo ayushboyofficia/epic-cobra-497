b950c6fddd1b984eb097d1df16ada86050a6083b
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:06:12 +0530

    style: fix comment alignment

--- Hack/database/__init__.py
+++ Hack/database/__init__.py
@@ -1,8 +1,2 @@
-from env import MONGO_URL
-from logger import LOGGER
-from Hack.database.mongo import Mongo
-
-DB = None
-if MONGO_URL:
-    LOGGER(__name__).info('Database Intialised')
-    DB = Mongo(MONGO_URL)
+"""Database package for SessionHack."""
+from .mongo import get_collection, insert, find, update
